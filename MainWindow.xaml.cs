fd220fcac60e7f96697404f1d1853dae5e8b1793
Florian Pallas <pallas.florian@gmail.com>
2019-12-26 19:11:39 +0100

    added visual output, added ability to find the shortest path

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,16 +16,316 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 namespace Abbiegen
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // CLASS MainWindow
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
     public partial class MainWindow : Window
     {
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        // GLOBAL VARIABLES
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        Point Startpoint;
+        Point Endpoint;
+        Point[,] Streets;
+        TStreetmap Streetmap;
+
+        // Drawing
+        private double Scale;
+        private double MinY;
+        private double MaxY;
+        private double MinX;
+        private double MaxX;
+        private const double Border = 5;
+        private double OffsetX = 0;
+        private double OffsetY = 0;
+
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        // CONSTRUCTOR
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        // METHODS
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        private void Calculate()
+        {
+            if (!Streetmap.CalculateShortestPath())
+            {
+                MessageBox.Show("Es konnte kein Weg bestimmt werden!", "Operation fehlgeschlagen", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            MessageBox.Show("Kürzester Weg erfolgreich berechnet.\n" + Streetmap.ShortestPath_Length.ToString("#.00") + "LE", "Operation erfolgreich", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        private void Draw()
+        {
+            if (Streetmap == null || Streetmap.Junctions == null || Streets == null) { return; }
+            CanvasMain.Children.Clear();
+
+            Draw_SetParameters();
+            Draw_Streets();
+            Draw_Path(Streetmap.ShortestPath, Brushes.Green, 0.05);
+            Draw_StartEnd();
+            Draw_Junctions();
+            Draw_StartEndText();
+        }
+
+        private void Draw_SetParameters()
+        {
+            // Determine real size of map
+            MinY = double.PositiveInfinity;
+            MaxY = double.NegativeInfinity;
+            MinX = double.PositiveInfinity;
+            MaxX = double.NegativeInfinity;
+
+            foreach (TJunction J in Streetmap.Junctions)
+            {
+                if (J.Location.Y > MaxY) { MaxY = J.Location.Y; }
+                if (J.Location.Y < MinY) { MinY = J.Location.Y; }
+                if (J.Location.X > MaxX) { MaxX = J.Location.X; }
+                if (J.Location.X < MinX) { MinX = J.Location.X; }
+            }
+
+            // Calculate scale and offset
+            double ScaleX = CanvasMain.ActualWidth / (MaxX - MinX + Border);
+            double ScaleY = CanvasMain.ActualHeight / (MaxY - MinY + Border);
+
+            Scale = Math.Min(ScaleX, ScaleY);
+
+            OffsetX = CanvasMain.ActualWidth - (MaxX - MinX) * Scale;
+            OffsetY = CanvasMain.ActualHeight - (MaxY - MinY) * Scale;
+        }
+
+        private void Draw_StartEnd()
+        {
+            DrawCircle(Startpoint, 0.25, Brushes.Blue);
+            DrawCircle(Endpoint, 0.25, Brushes.Blue);
+        }
+
+        private void Draw_StartEndText()
+        {
+            DrawText(Startpoint, "S", Brushes.Blue, 0.15);
+            DrawText(Endpoint, "E", Brushes.Blue, 0.15);
+        }
+
+        private void Draw_Junctions()
+        {
+            foreach (TJunction J in Streetmap.Junctions)
+            {
+                DrawCircle(J.Location, 0.2, Brushes.LightGray);
+            }
+        }
+
+        private void Draw_Streets()
+        {
+            for(int I = 0; I < Streets.GetLength(0); I++)
+            {
+                Point A = Streets[I, 0];
+                Point B = Streets[I, 1];
+                DrawLine(A, B, Brushes.LightGray, 0.05);
+            }
+        }
+
+        public void Draw_Path(TJunction[] Path, Brush Color, double Thickness)
+        {
+            if(Path == null) { return; }
+
+            for (int I = 1; I < Path.Length; I++)
+            {
+                Point A = Path[I - 1].Location;
+                Point B = Path[I].Location;
+                DrawLine(A, B, Color, Thickness);
+            }
+        }
+
+        public void DrawText(Point P, string Text, Brush Color, double Size)
+        {
+            P = PointToCanvas(P);
+
+            TextBlock TextBlock = new TextBlock()
+            {
+                Text = Text,
+                FontSize = Size * Scale,
+                Foreground = Color
+            };
+
+            TextBlock.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+
+            Canvas.SetLeft(TextBlock, P.X - TextBlock.DesiredSize.Width * 0.5);
+            Canvas.SetTop(TextBlock, P.Y - TextBlock.DesiredSize.Height * 0.5);
+
+            CanvasMain.Children.Add(TextBlock);
+        }
+
+        public void DrawLine(Point A, Point B, Brush Color, double Thickness)
+        {
+            A = PointToCanvas(A);
+            B = PointToCanvas(B);
+
+            Line Linie = new Line()
+            {
+                Stroke = Color,
+                X1 = A.X,
+                Y1 = A.Y,
+                X2 = B.X,
+                Y2 = B.Y,
+                StrokeThickness = Scale * Thickness
+            };
+
+            CanvasMain.Children.Add(Linie);
+        }
+
+        private void DrawCircle(Point P, double Radius, Brush Farbe)
+        {
+            P = PointToCanvas(P);
+            Radius *= Scale;
+
+            Ellipse E = new Ellipse()
+            {
+                Width = Radius,
+                Height = Radius,
+                Fill = Farbe
+            };
+
+            CanvasMain.Children.Add(E);
+
+            Canvas.SetLeft(E, P.X - Radius * 0.5);
+            Canvas.SetTop(E, P.Y - Radius * 0.5);
+        }
+
+        private Point PointToCanvas(Point P)
+        {
+            return new Point((P.X - MinX) * Scale + OffsetX / 2, CanvasMain.ActualHeight - P.Y * Scale - OffsetY / 2);
+        }
+
+        // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        private void OpenFile()
+        {
+            // Open file dialog
+            Microsoft.Win32.OpenFileDialog Dlg = new Microsoft.Win32.OpenFileDialog()
+            {
+                Filter = "\"Abbiegen\"-Datei (*.txt)|*.txt|Alle Dateien (*.*)|*.*",
+                FilterIndex = 0
+            };
+
+            // Return if user cancels selection
+            if (Dlg.ShowDialog() != true)
+            {
+                return;
+            }
+
+            // Read file contents
+            FileStream File = new FileStream(Dlg.FileName, FileMode.Open);
+            string FileString = String.Empty;
+            StreamReader Reader = new StreamReader(File);
+
+            try
+            {
+                FileString = Reader.ReadToEnd();
+            }
+            catch
+            {
+                MessageBox.Show("Die Datei konnte nicht eingelesen werden.");
+                return;
+            }
+            finally
+            {
+                File.Close();
+                Reader.Close();
+            }
+
+            // Split file into rows
+            string[] Rows = FileString.Split('\n');
+
+            try
+            {
+                // Parse file
+                int StreetCount = int.Parse(Rows[0]);
+                Startpoint = PointFromString(Rows[1]);
+                Endpoint = PointFromString(Rows[2]);
+                Streets = new Point[StreetCount, 2];
+
+                for(int I = 0; I < StreetCount; I++)
+                {
+                    string[] Substrings = Rows[3 + I].Split(' ');
+                    Streets[I, 0] = PointFromString(Substrings[0]);
+                    Streets[I, 1] = PointFromString(Substrings[1]);
+                }
+
+                // Generate streetmap
+                Streetmap = new TStreetmap(Streets, Startpoint, Endpoint);
+
+                MessageBox.Show("Die Datei wurde erfolgreich eingelesen.", "Operation erfolgreich", MessageBoxButton.OK, MessageBoxImage.Information);
+                MenuItemCalculate.IsEnabled = true;
+            }
+            catch
+            {
+                MessageBox.Show("Die Datei konnte nicht eingelesen werden!", "Operation fehlgeschlagen", MessageBoxButton.OK, MessageBoxImage.Error);
+                MenuItemCalculate.IsEnabled = false;
+            }
+        }
+
+        private Point PointFromString(string S)
+        {
+            S = S.Replace("(", "").Replace(")", "").Trim();
+            string[] Coordinates = S.Split(',');
+            Point P = new Point();
+            P.X = int.Parse(Coordinates[0]);
+            P.Y = int.Parse(Coordinates[1]);
+            return P;
+        }
+
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        // HANDLERS
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        private void MenuItemCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            Calculate();
+            Draw();
+        }
+
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        private void MenuItemFileOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFile();
+            Draw();
+        }
+
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        private void MenuItemFileExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Draw();
+        }
+
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     }
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 }
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
---
+++ Streetmap.cs
@@ -0,0 +1,226 @@
+﻿// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+namespace Abbiegen
+{
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // CLASS TStreetmap
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    public class TStreetmap
+    {
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        // GLOBAL VARIABLES
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        public TJunction[] Junctions;
+        public TJunction StartJunction;
+        public TJunction EndJunction;
+
+        private List<TJunction> _ShortestPath;
+        public TJunction[] ShortestPath;
+        public double ShortestPath_Length;
+
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        // CONSTRUCTOR
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        public TStreetmap(Point[,] _Streets, Point _Startpoint, Point _Endpoint)
+        {
+            // Reset variables
+            Junctions = null;
+            StartJunction = null;
+            EndJunction = null;
+            ShortestPath = null;
+            ShortestPath_Length = 0.0;
+
+            // Generate streetmap
+            GenerateFromStreets(_Streets, _Startpoint, _Endpoint);
+        }
+
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        // METHODS
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        private void GenerateFromStreets(Point[,] _Streets, Point _Startpoint, Point _Endpoint)
+        {
+            List<TJunction> _Junctions = new List<TJunction>();
+
+            // Create junctions
+            for(int I = 0; I < _Streets.GetLength(0); I++)
+            {
+                int Index;
+                Point A = _Streets[I, 0];
+                Point B = _Streets[I, 1];
+
+                Index = _Junctions.FindIndex(X => X.Location == A);
+                if (Index == -1)
+                {
+                    _Junctions.Add(new TJunction() { Location = A });
+                }
+
+                Index = _Junctions.FindIndex(X => X.Location == B);
+                if (Index == -1)
+                {
+                    _Junctions.Add(new TJunction() { Location = B });
+                }
+            }
+
+            // Set neighbor junctions
+            for (int I = 0; I < _Streets.GetLength(0); I++)
+            {
+                Point A = _Streets[I, 0];
+                Point B = _Streets[I, 1];
+
+                int IndexA = _Junctions.FindIndex(X => X.Location == A);
+                int IndexB = _Junctions.FindIndex(X => X.Location == B);
+                double Length = new Vector(B.X - A.X, B.Y - A.Y).Length;
+
+                _Junctions[IndexA].Neighbors.Add(_Junctions[IndexB]);
+                _Junctions[IndexA].NeighborValues.Add(Length);
+                _Junctions[IndexB].Neighbors.Add(_Junctions[IndexA]);
+                _Junctions[IndexB].NeighborValues.Add(Length);
+            }
+
+            // Find junction at start and end
+            int IndexStart = _Junctions.FindIndex(X => X.Location == _Startpoint);
+            StartJunction = _Junctions[IndexStart];
+
+            int IndexEnd = _Junctions.FindIndex(X => X.Location == _Endpoint);
+            EndJunction = _Junctions[IndexEnd];
+
+            // Store in global array
+            Junctions = _Junctions.ToArray();
+        }
+
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        public bool CalculateShortestPath()
+        {
+            /* DIJKSTRA ALGORITHM */
+
+            // Store unvisited junctions
+            List<TJunction> Remaining = new List<TJunction>();
+
+            // Set defaults
+            foreach (TJunction J in Junctions)
+            {
+                J.Distance = double.PositiveInfinity;
+                J.PreVertex = null;
+                Remaining.Add(J);
+            }
+
+            StartJunction.Distance = 0;
+
+            while (Remaining.Count > 0)
+            {
+                // Find next junction (closest)
+                TJunction Current = ClosestJunction(Remaining);
+
+                // Remove current junction from calculation
+                Remaining.Remove(Current);
+
+                // Finish
+                if (Current == EndJunction)
+                {
+                    GetShortestPath();
+                    ShortestPath = _ShortestPath.ToArray();
+                    return true;
+                }
+
+                // Update neighbor values
+                foreach (TJunction Neighbor in Current.Neighbors)
+                {
+                    double Alt = Current.Distance + Current.DistanceTo(Neighbor);
+                    if (Alt < Neighbor.Distance)
+                    {
+                        Neighbor.Distance = Alt;
+                        Neighbor.PreVertex = Current;
+                    }
+                }
+            }
+
+            // Return false if no path is found
+            return false;
+        }
+
+        private void GetShortestPath()
+        {
+            ShortestPath_Length = 0.0;
+
+            List<TJunction> S = new List<TJunction>();
+            TJunction Current = EndJunction;
+
+            if (Current.PreVertex != null || Current == StartJunction)
+            {
+                while (Current != null)
+                {
+                    S.Add(Current);
+                    if(Current != StartJunction)
+                    {
+                        ShortestPath_Length += Current.DistanceTo(Current.PreVertex);
+                    }
+                    Current = Current.PreVertex;
+                }
+            }
+
+            S.Reverse();
+            _ShortestPath = S;
+        }
+
+        private TJunction ClosestJunction(List<TJunction> Q)
+        {
+            TJunction ClosestJunction = null;
+            foreach (TJunction V in Q)
+            {
+                if (ClosestJunction == null || V.Distance < ClosestJunction.Distance)
+                {
+                    ClosestJunction = V;
+                }
+            }
+
+            return ClosestJunction;
+        }
+
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    }
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // CLASS TJunction
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    public class TJunction
+    {
+        public TJunction()
+        {
+            Neighbors = new List<TJunction>();
+            NeighborValues = new List<double>();
+        }
+
+        public Point Location;
+        public List<TJunction> Neighbors;
+        public List<double> NeighborValues;
+
+        public TJunction PreVertex;
+        public double Distance;
+
+        public double DistanceTo(TJunction _Neighbor)
+        {
+            int Index = Neighbors.FindIndex(x => x == _Neighbor);
+
+            return NeighborValues[Index];
+        }
+    }
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+}
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
